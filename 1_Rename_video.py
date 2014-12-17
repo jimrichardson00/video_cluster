338db46f33a7691ab56f0f8ced6229bc1eb1479c
Jim Richardson <jimrichardson00>
2014-12-16 16:41:14 -0800

    modified code to work as a function rather that straight code. can enter arguments from_dir and to_dir. this will be useful when we deal with multiple sets of video data eg. DFO2013, DFO2014, etc

--- 1_Rename_video.py
+++ 1_Rename_video.py
@@ -10,101 +10,118 @@ import shutil
 from_dir = '/home/jim/Desktop/DFO Survey - October 2013 - copy/october 2013 video'
 to_dir = '/home/jim/Desktop/pca_video/video'
 
-#sets empty lists to be used later for the files and folders in directory
-folders = []
-years = []
+def rename_video( from_dir, to_dir ):
 
-#cycles through folders in directory
-for folder in os.listdir(from_dir):
-  #sets the full path of each folder
-  folder_path = os.path.abspath(os.path.join(from_dir,folder))
-  #sets root and extension of each folder
-  folderRoot, fileExtension = os.path.splitext(folder_path)
-  if fileExtension == '':
-    #if file extension is empty (i.e. a folder), adds the file name to list of file names
-    folders.append(folder)
-    print folder
+  #sets empty lists to be used later for the files and folders in directory
+  folders = []
+  years = []
 
-# f = open("/home/jim/Dropbox/Records/Filenames.txt", "r")
-# Filenames_txt = f.read().split('\n')
-# f.close()
+  #cycles through folders in directory
+  for folder in os.listdir(from_dir):
+    #sets the full path of each folder
+    folder_path = os.path.abspath(os.path.join(from_dir,folder))
+    #sets root and extension of each folder
+    folderRoot, fileExtension = os.path.splitext(folder_path)
+    if fileExtension == '':
+      #if file extension is empty (i.e. a folder), adds the file name to list of file names
+      folders.append(folder)
+      print folder
 
-for folder in folders:
+  # f = open("/home/jim/Dropbox/Records/Filenames.txt", "r")
+  # Filenames_txt = f.read().split('\n')
+  # f.close()
 
-  print folder
+  for folder in folders:
 
-  # folder path
-  folder_path = os.path.abspath(os.path.join(from_dir,folder))
-  #sets root and extension of each folder
-  folderRoot, fileExtension = os.path.splitext(folder_path)
+    # folder path
+    folder_path = os.path.abspath(os.path.join(from_dir,folder))
+    #sets root and extension of each folder
+    folderRoot, fileExtension = os.path.splitext(folder_path)
 
-  # extract set and trap
-  match_ST = re.search(r'Set(\d+)Trap(\d+)', folder)
+    # extract set and trap
+    # match_ST = re.search(r'Set(\d+)Trap(\d+)', folder)
+    match_ST = re.search(r'[a-zA-Z]+(\d+)[a-zA-Z]+(\d+)', folder)
 
-  print match_ST
+    Set = match_ST.group(1)
+    Trap = match_ST.group(2)
 
-  Set = match_ST.group(1)
-  Trap = match_ST.group(2)
+    Set = str(Set).zfill(3)
+    Trap = str(Trap).zfill(3)
 
-  Set = str(Set).zfill(3)
-  Trap = str(Trap).zfill(3)
+    # ----------------------------------------------------------------------
+    # first video in each set/trap
 
-  print Set
-  print Trap
+    filenames = os.listdir(folder_path)
+    filenames.sort()
+    filename = filenames[0]
 
-  print os.listdir(folder_path)[0]
-  print ' '
+    # extract filename path
+    filename_path = os.path.abspath(os.path.join(folder_path, filename))
+    # extract filename extension
+    fileRoot, fileExtension = os.path.splitext(filename_path)
+    print filename
+    if fileExtension == '.MP4':
 
-  filenames = os.listdir(folder_path)
-  filenames.sort()
-  filename = filenames[0]
+      # copies the file
+      shutil.copy(filename_path, to_dir)
 
-  # extract filename path
-  filename_path = os.path.abspath(os.path.join(folder_path, filename))
-  # extract filename extension
-  fileRoot, fileExtension = os.path.splitext(filename_path)
-  print filename
-  if fileExtension == '.MP4':
+      # new filename
+      NewFilename = 'DFO2013' + '_' + 'S' + str(Set) + 'T' + str(Trap) + '_' + str(filename)
+      print NewFilename
 
-    # copies the file
-    shutil.copy(filename_path, to_dir)
+      # new filename path
+      NewFilename_path = os.path.abspath(os.path.join(to_dir, NewFilename))
 
-    # new filename
-    NewFilename = 'DFO2013' + '_' + 'S' + str(Set) + 'T' + str(Trap) + '_' + str(filename)
-    print NewFilename
+      # full path of copied file
+      filename_path = os.path.abspath(os.path.join(to_dir, filename))
 
-    # new filename path
-    NewFilename_path = os.path.abspath(os.path.join(to_dir, NewFilename))
+      # renames the file
+      os.rename(filename_path, NewFilename_path)
 
-    # full path of copied file
-    filename_path = os.path.abspath(os.path.join(to_dir, filename))
+      # ----------------------------------------------------------------
+      # all videos in set/trap
 
-    # renames the file
-    os.rename(filename_path, NewFilename_path)
+    # # may need to use whole path for folder
+    # for filename in filenames:
 
+    #   # extract filename path
+    #   filename_path = os.path.abspath(os.path.join(folder_path, filename))
 
+    #   # extract filename extension
+    #   fileRoot, fileExtension = os.path.splitext(filename_path)
 
-  # # may need to use whole path for folder
-  # for filename in os.listdir(folder_path):
+    #   print filename
 
-  #   # extract filename path
-  #   filename_path = os.path.abspath(os.path.join(folder_path, filename))
+    #   if fileExtension == '.MP4':
 
-  #   # extract filename extension
-  #   fileRoot, fileExtension = os.path.splitext(filename_path)
+    #   # copies the file
+    #   shutil.copy(filename_path, to_dir)
 
-  #   print filename
+    #   # new filename
+    #   NewFilename = 'DFO2013' + '_' + 'S' + str(Set) + 'T' + str(Trap) + '_' + str(filename)
+    #   print NewFilename
 
-  #   if fileExtension == '.MP4':
+    #   # new filename path
+    #   NewFilename_path = os.path.abspath(os.path.join(to_dir, NewFilename))
 
-  #     NewFilename = 'DFO2013' + '_' + 'S' + str(Set) + 'T' + str(Trap) + '_' + str(filename)
-  #     print NewFilename
+    #   # full path of copied file
+    #   filename_path = os.path.abspath(os.path.join(to_dir, filename))
 
-  #     # new filename path
-  #     NewFilename_path = os.path.abspath(os.path.join(folder_path, NewFilename))
+    #   # renames the file
+    #   os.rename(filename_path, NewFilename_path)
 
-  #     #renames the file
-  #     os.rename(filename_path, NewFilename_path)
+def main():
 
-  #     # moves the file
-  #     shutil.move(NewFilename_path, to_dir)
+  if len(sys.argv) != 3:
+    print 'usage: python 1_Rename_video.py from_dir to_dir'
+    sys.exit(1)
+
+  from_dir = sys.argv[1]
+  to_dir = sys.argv[2]
+
+  rename_video( from_dir, to_dir )
+  
+  sys.exit(1)
+
+if __name__ == '__main__':
+  main()
