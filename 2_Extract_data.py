d00fd0f1fa72091b1e4feac09fe100592465b640
Jim Richardson <jimrichardson00>
2014-12-16 16:46:03 -0800

    modified code to work as a function, that takes the name of a .mp4 file as an argument. also added code to take top middle chunk of frames, since this will have no trap in it (causes problems with clustering - traps in different places in video

--- 2_Extract_data.py
+++ 2_Extract_data.py
@@ -4,12 +4,16 @@ import os
 import os.path
 import re
 import shutil
+import sys
+import math
+from decimal import *
 
-video_dir = '/home/jim/Desktop/pca_video/video'
-data_dir = '/home/jim/Desktop/pca_video/data'
-frames_dir = '/home/jim/Desktop/pca_video/frames'
 
-for filename in os.listdir(video_dir):
+video_dir = '/home/jim/Desktop/video_cluster/video'
+data_dir = '/home/jim/Desktop/video_cluster/data'
+frames_dir = '/home/jim/Desktop/video_cluster/frames'
+
+def extract_data( filename ):
 
   #sets the full path of each folder
   filename_path = os.path.abspath(os.path.join(video_dir,filename))
@@ -36,6 +40,16 @@ for filename in os.listdir(video_dir):
       
       if f % 30 == 0:
 
+        height, width, depth = image.shape
+        print height, width, depth
+
+        x1 = math.floor(Decimal(0.26041666666)*Decimal(width))
+        y1 = math.floor(Decimal(0.55555555555)*Decimal(height))
+
+        x2 = math.floor(Decimal(0.73958333334)*Decimal(width))
+        y2 = math.floor(Decimal(0.55555555555)*Decimal(height))
+
+        image = image[0:y1, x1:x2]
         image = cv2.resize(image, (192, 108))
 
         # output frames
@@ -49,3 +63,16 @@ for filename in os.listdir(video_dir):
         numpy.savetxt(fileRoot + "_" + "F" + str(f).zfill(4) + "_" + 'red' + '.txt', image[:,:,2])
 
       f += 1
+
+def main():
+
+  if len(sys.argv) != 2:
+    print 'usage: python 2_Extract_data.py FILENAME.MP4'
+    sys.exit(1)
+
+  filename = sys.argv[1]
+  extract_data( filename )
+  sys.exit(1)
+
+if __name__ == '__main__':
+  main()
