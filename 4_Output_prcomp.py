4e00752b8b9b96c2cc14d13708cfdbbbfc6d543f
Jim Richardson <jimrichardson00>
2014-12-17 11:19:41 -0800

    modified code to add variable n, which is the desired number of principal component images to write to jpg. code runs through a four-loop of length n. the txt files must first be created by 4_Apply_clustering.r for this to work, so it is better to run this as a system call within 4_Apply_clustering.r

--- 4_Output_prcomp.py
+++ 4_Output_prcomp.py
@@ -1,42 +1,40 @@
 import cv2
 import numpy
-import cv
+import os
+import os.path
+import re
+import shutil
+import sys
+import math
+from decimal import *
 
-# pc1
-image_pc1_blu = numpy.loadtxt("image_pc1_blu.txt")
-image_pc1_gre = numpy.loadtxt("image_pc1_gre.txt")
-image_pc1_red = numpy.loadtxt("image_pc1_red.txt")
+def output_prcomp( n ):
 
-image_pc1 = numpy.zeros((108, 192, 3), numpy.uint8)
+  for i in range(1, int(n)):
 
-image_pc1[:, :, 0] = image_pc1_blu
-image_pc1[:, :, 1] = image_pc1_gre
-image_pc1[:, :, 2] = image_pc1_red
+    image_pc_blu = numpy.loadtxt("image_pc" + str(i) + "_blu.txt")
+    image_pc_gre = numpy.loadtxt("image_pc" + str(i) + "_gre.txt")
+    image_pc_red = numpy.loadtxt("image_pc" + str(i) + "_red.txt")
 
-cv2.imwrite("image_pc1.jpg", image_pc1)
+    image_pc = numpy.zeros((108, 192, 3), numpy.uint8)
 
-# pc2
-image_pc2_blu = numpy.loadtxt("image_pc2_blu.txt")
-image_pc2_gre = numpy.loadtxt("image_pc2_gre.txt")
-image_pc2_red = numpy.loadtxt("image_pc2_red.txt")
+    image_pc[:, :, 0] = image_pc_blu
+    image_pc[:, :, 1] = image_pc_gre
+    image_pc[:, :, 2] = image_pc_red
 
-image_pc2 = numpy.zeros((108, 192, 3), numpy.uint8)
+    cv2.imwrite("image_pc" + str(i) + ".jpg", image_pc)
 
-image_pc2[:, :, 0] = image_pc2_blu
-image_pc2[:, :, 1] = image_pc2_gre
-image_pc2[:, :, 2] = image_pc2_red
+def main():
 
-cv2.imwrite("image_pc2.jpg", image_pc2)
+  if len(sys.argv) != 2:
+    print 'usage: python 4_Output_prcomp.py n'
+    sys.exit(1)
 
-# pc3
-image_pc3_blu = numpy.loadtxt("image_pc3_blu.txt")
-image_pc3_gre = numpy.loadtxt("image_pc3_gre.txt")
-image_pc3_red = numpy.loadtxt("image_pc3_red.txt")
+  n = sys.argv[1]
 
-image_pc3 = numpy.zeros((108, 192, 3), numpy.uint8)
+  output_prcomp( n )
+  
+  sys.exit(1)
 
-image_pc3[:, :, 0] = image_pc3_blu
-image_pc3[:, :, 1] = image_pc3_gre
-image_pc3[:, :, 2] = image_pc3_red
-
-cv2.imwrite("image_pc3.jpg", image_pc3)
+if __name__ == '__main__':
+  main()
