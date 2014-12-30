985e0a1be32a8d4db4415f6715997af1b7672a71
Jim Richardson <jimrichardson00>
2014-12-30 01:47:35 -0800

    intial commit. function within 3_Apply_clustering called Output_image.r takes data file of length 3*W*H, with (b, g, r, b, g, r, ...), with by rows, moving left to right, and outputs it into matrices blue, green, red. python code called Output_image.py, takes this and outputs .jpg to given dest_dir

---
+++ Output_image.py
@@ -0,0 +1,22 @@
+import cv2
+from sklearn.decomposition import PCA
+from pyIPCA import CCIPCA
+from os.path import join, exists
+import urllib
+import zipfile
+import sys
+import os
+from decimal import *
+import math
+import numpy
+
+def Output_image( name, red, gre, blu, dest_dir, W, H ):
+
+  image = numpy.zeros((H, W, 3), numpy.uint8)
+  
+  image[:, :, 0] = blu
+  image[:, :, 1] = gre
+  image[:, :, 2] = red
+  
+  os.chdir(str(dest_dir))
+  cv2.imwrite(str(name) + ".jpg", image)
