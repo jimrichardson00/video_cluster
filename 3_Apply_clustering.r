adf0f5ca7d13038640ac281bde7ed0af4d786e8d
Jim Richardson <jimrichardson00>
2015-01-06 12:18:17 -0800

    changed code so tat it can be run on someone else's computer (defined directorys at start, master_dir, video_dir etc). also modified function CCIPCA_SetTrap, added function Video_mod, diff_from_mod. CCIPCA_SetTrap now calls CCIPCA_SetTrap.py which takes a set and trap, runs PCA on all frames, outputs transformed data prx. The R function CCIPCA_SetTrap then calculates the difference from mode (like variance but with mod instead of mean) for each video. The idea is then to select the video with the minimum difference from mode for a given settrap - this should be a still, non cloudy shot of the bottom (hopefully). CCIPCA_SetTrap outputs table with SetTrap, video_file, Video_mod. Then we can run mclapply in parallel to return a list with this table for each SetTrap.

--- 3_Apply_clustering.r
+++ 3_Apply_clustering.r
@@ -1,24 +1,30 @@
-library(dendextend)
-library(dendroextras)
-library(plot3D)
-library(rgl)
-library(plyr)
-library(stringr)
-library(NbClust)
-library(modeest)
-library(scatterplot3d)
-library(beepr)
-library(rPython)
-library(MASS)
-library(Hmisc)
-library(beepr)
+require(dendextend)
+require(dendroextras)
+require(plot3D)
+require(rgl)
+require(plyr)
+require(stringr)
+require(NbClust)
+require(modeest)
+require(scatterplot3d)
+require(beepr)
+require(rPython)
+require(MASS)
+require(Hmisc)
+require(beepr)
+require(plyr)
+require(parallel)
+require(doParallel)
+require(foreach)
 
+n_cores = detectCores(all.tests = FALSE, logical = FALSE) - 1
+n_cores
 n_components = 100
 W = 192
 H = 108
-frames_dir = "/home/jim/Desktop/video_cluster/rep_frames"
-video_dir = "/home/jim/Desktop/video_cluster/video"
-master_dir = "/home/jim/Desktop/video_cluster"
+frame_dir = "/home/jim/Dropbox/REM/Tasks/video_cluster/frame"
+video_dir = "/home/jim/Dropbox/REM/Tasks/video_cluster/video"
+master_dir = "/home/jim/Dropbox/REM/Tasks/video_cluster"
 
 posVar <- function(data) {
 	out <- lapply(data, function(x) length(unique(x)))
@@ -26,14 +32,11 @@ posVar <- function(data) {
 	unlist(want)
 }
 
-extract_data <- function(DFO2013_SiiiTjjj_GP) {
-	setwd("/home/jim/Desktop/video_cluster/")
-	system(paste("python 2_Extract_data.py ", DFO2013_SiiiTjjj_GP, ".MP4", sep = ""))
-}
-
 CCIPCA_video <- function(video_file, n_components, W, H, fast) {
 
-	setwd("/home/jim/Desktop/video_cluster/")
+	require(rPython)
+
+	setwd(master_dir)
 	python.load("CCIPCA_video.py")
 	output <- python.call("CCIPCA_video", video_file, n_components, W, H, fast)
 
@@ -56,93 +59,56 @@ CCIPCA_video <- function(video_file, n_components, W, H, fast) {
 
 		rep_idx <- seq(1, nrow(prx), by = 1)[cl == mfv(cl)][1]
 
-	} else {
+	} else if(fast == 1) {
+
 		rep_idx <- 1
+
 	}
-	return(t(framesData)[, rep_idx])
+
+	if(fast == 0 | fast == 1){
+		return(framesData[rep_idx,])
+	} else if (fast == 3){
+		return(framesData)
+	}
 }
 
-setwd("/home/jim/Desktop/video_cluster/video")
-list.files()
-video_file <- "STRS2013_S004T014_GOPR0606.MP4"
-SetTrap <- str_match(video_file, "(.+)_GOPR(.+)")[, 2]
-SetTrap
+Video_var <- function(video_file, W, H, skip = 6) {
 
-n_components = 20
-W = 192
-H = 108
-3*W*H
+	print(video_file)
 
-CCIPCA_SetTrap <- function(SetTrap, n_components, W, H, fast) {
+	setwd(master_dir)
+	python.load("Video_var.py")
+	Video_var <- python.call("Video_var", video_file, W, H, skip)
 
-	setwd("/home/jim/Desktop/video_cluster/")
-	python.load("CCIPCA_SetTrapVar.py")
-	output <- python.call("CCIPCA_SetTrapVar", video_file, n_components, W, H)
-	python.call("CCIPCA_SetTrapVar", video_file, n_components, W, H)
-	output
-	beep()
+	return(Video_var)
+}
 
-	framesData[[1]]
-	components_[[1]]
+
+video_file = "STRS2013_S087T014_GOPR0411.MP4"
+Video_mod <- function(video_file, W, H, skip = 6) {
+
+	fast = 0
+
+	setwd(master_dir)
+	python.load("CCIPCA_video.py")
+	output <- python.call("CCIPCA_video", video_file, n_components, W, H, skip, fast)
 
 	framesData <- matrix(unlist(output[[1]]), ncol = 3*W*H, byrow = TRUE)
 	components_ <- matrix(unlist(output[[2]]), ncol = 3*W*H, byrow = TRUE)
-	video_files_frames <- output[[3]]
+
 	prx <- t(components_ %*% t(framesData))
 
-	nrow(prx)
-	ncol(prx)
-
-	# optimal number of cluster
-	# NbClust <- NbClust(data = prx[, 1:2], 
-	# 	method = 'kmeans', min.nc = 2, max.nc = nrow(prx) - 2,
-	# 	index = 'kl')
-	# NbClust <- NbClust$Best.nc[1]
- 	NbClust <- round(sqrt(nrow(prx)/2), 0)
- 	NbClust
-
-	fit <- kmeans(prx, centers = NbClust)
-	clusters <- fit$cluster
-	clusters
-
-	plot(prx[, 1:2], col = clusters)
-	points(fit$centers, pch = 16)
-	text(fit$centers[, 1:2], paste(unique(clusters)), pos = 2)
-	
-	plot3d(prx[, 1:3])
-
-	data = t(t(components_) %*% fit$centers[2, ])
-	fit$centers
-	ncol(components_)
-	nrow(components_)
-	
-	unique(clusters)[min(fit$withinss) == fit$withinss]
-
-	for(i in seq(1, length(video_files_frames), 1)) {		
-		name = paste(clusters[i], "_", video_files_frames[i], sep = "")
-		data = framesData[i, ]
-		dest_dir = frames_dir
-		W = 192
-		H = 108
-		Output_image(name, data, dest_dir, W, H)
-
-		name = paste(clusters[i], sep = "")
-		data = t(t(components_) %*% fit$centers[clusters[i], ])
-		dest_dir = frames_dir
-		W = 192
-		H = 108
-		Output_image(name, data, dest_dir, W, H)
+	for(j in seq(1, ncol(prx), 1)) {
+		mode = density(prx[, j])$x[which.max(density(prx[, j])$y)]
+		prx[, j] <- prx[, j] - mode
 	}
 
-	rep_idx <- seq(1, nrow(prx), by = 1)[clusters == mfv(clusters)][1]
-	rep_idx
-
-	return(t(framesData)[, rep_idx])
+	return(mean(apply(prx, MARGIN = 2, FUN = function(x) mean(sum(x^2)))))
 }
 
 CCIPCA_RepFrames <- function(RepFrames_new, n_components, rerun) {
 
-	setwd("/home/jim/Desktop/video_cluster")
+	setwd(master_dir)
 
 	RepFrames_new <- matrix(unlist(RepFrames_new[1:nrow(RepFrames_new),1:ncol(RepFrames_new)]), nrow = nrow(RepFrames_new), byrow = FALSE)
 
@@ -166,9 +132,48 @@ CCIPCA_RepFrames <- function(RepFrames_new, n_components, rerun) {
 
 }
 
+SetTrap = "STRS2013_S005T014"
+skip = 6
+n_components = 700
+W = 192
+H = 108
+CCIPCA_SetTrap <- function(SetTrap, W = 192, H = 108, skip = 6, video_dir = video_dir) {
+
+	print(paste("Starting: ", SetTrap, sep = ""))
+
+	setwd(master_dir)
+	python.load("CCIPCA_SetTrap.py")
+	output <- python.call("CCIPCA_SetTrap", SetTrap, W, H, skip, video_dir)
+	prx <- matrix(unlist(output[[1]]), ncol = sqrt(length(unlist(output[[1]]))), byrow = TRUE)
+	video_files_frames <- output[[2]]
+
+	min = min(as.numeric(str_match(video_files_frames, "(.+)_GOPR0(.+)_F(.+)")[, 3]))
+
+	cols <- as.numeric(str_match(video_files_frames, "(.+)_GOPR0(.+)_F(.+)")[, 3]) - min + 1
+
+	video_files <- na.omit(str_match(video_files_frames, paste("(", SetTrap, ".+)", "_F.+", sep = ""))[, 2])
+
+	video_files_mod <- unlist(lapply(sort(unique(video_files)), FUN = function(video_file) diff_from_mode(prx[video_files == video_file, ])))
+
+	setwd(master_dir)
+	png(paste(SetTrap, ".png", sep = ""))
+	plot(prx[, 1:2], col = cols, main = SetTrap)
+	legend("topright", fill = sort(unique(cols)),
+	 legend = paste(unique(sort(cols)) + min - 1, " ", 
+			"Mod-", formatC(video_files_mod, flag = "0", width = 9, format = "d"), sep = ""))
+	dev.off()
+
+	m <- as.matrix(data.frame(SetTrap = rep(SetTrap, length(unique(video_files))), video_files = sort(unique(video_files)), video_files_mod = video_files_mod), ncol = 3, drop = FALSE)
+
+	print(paste("Finished: ", SetTrap, sep = ""))
+
+	return(m)
+
+}
+
 Output_image <- function(name, data, dest_dir, W, H) {
 
-	setwd("/home/jim/Desktop/video_cluster")
+	setwd(master_dir)
 
 	data <- as.vector(data)
 	N <- length(data)/3
@@ -179,6 +184,14 @@ Output_image <- function(name, data, dest_dir, W, H) {
 	
 	python.load("Output_image.py")
 	python.call("Output_image", name, red, gre, blu, dest_dir, W, H)
+}
+
+diff_from_mode <- function(prx) {
+	for(j in seq(1, ncol(prx), 1)) {
+		modej = density(prx[, j])$x[which.max(density(prx[, j])$y)]
+		prx[, j] <- prx[, j] - modej
+	}
+	return(mean(apply(prx, MARGIN = 2, FUN = function(x) mean(sum(x^2)))))
 }
 
 # ----------------------------------------------------------------
@@ -200,8 +213,8 @@ for(i in seq(1, length(from_dirs),1 )) {
 
 W = 192
 H = 108
-video_dir = "/home/jim/Desktop/video_cluster/video"
-master_dir = "/home/jim/Desktop/video_cluster"
+master_dir = "/home/jim/Dropbox/REM/Tasks/video_cluster"
+video_dir = "/home/jim/Dropbox/REM/Tasks/video_cluster/vide"
 setwd(master_dir)
 python.load("Add_weighted.py")
 python.call("Add_weighted", video_dir, master_dir, W, H)
@@ -224,42 +237,80 @@ if(file.exists("ccipca.RData") == TRUE) {
 	components_ = ccipca[["components_"]] 
 	RepFrames_cur = ccipca[["RepFrames_cur"]] 
 	video_files_cur = ccipca[["video_files_cur"]]
+	SetTraps_cur = ccipca[["SetTraps"]]
 
 } else {
 	
 	RepFrames_cur <- data.frame()
 	video_files_cur <- vector()
+	SetTraps_cur <- vector()
 
 }
 
-# list of videos in video folder
-setwd("/home/jim/Desktop/video_cluster/video")
+# list of videos and SetTraps in video folder
+setwd(video_dir)
 video_files <- list.files()
 video_files
+SetTraps <- unique(str_match(sort(list.files(video_dir)), "(.+)_GOPR")[, 2])
+SetTraps
 
 # defines new videos to be added
 video_files_new <- video_files[!(video_files %in% video_files_cur)]
 video_files_new
+SetTraps_new <- SetTraps[!(SetTraps %in% SetTraps_cur)]
+SetTraps_new
 
 # calculates new rep frames
 video_file <- video_files_new[124]
 video_file
 
-# cacluates the new rep frames matrix
-RepFrames_new <- matrix(NA, nrow = length(video_files_new), ncol = 3*H*W)
-if(nrow(RepFrames_new) > 0) {
-	for(i in seq(1, length(video_files_new), 1)) {
-		video_file <- video_files_new[i]
-		print(paste(video_file, " - ", i, sep = ""))
-		RepFrame <- CCIPCA_video(video_file = video_file, n_components = 10, W = W, H = H, fast = 0)
-		RepFrames_new[i, ] <- RepFrame
-	}
+# calculates difference from mode for each video file
+# Video_var_v <- unlist(mclapply(video_files, FUN = function(video_file) Video_var(video_file = video_file, W = W, H = H, skip = 6), mc.cores = n_cores))
+Video_var_L <- mclapply(SetTraps, FUN = function(SetTrap) CCIPCA_SetTrap(SetTrap = SetTrap, W = W, H = H, skip = 6, video_dir = video_dir), mc.cores = n_cores)
+Video_var_L
+
+Video_var_df <- data.frame(video_files = video_files, SetTrap = str_match(video_files, "(.+)_GOPR(.+)")[, 2], Video_var = Video_var_v)
+Video_var_min <- ddply(Video_var_df, .(SetTrap), summarise, Min_video_var = min(Video_var), Min_video_file = video_files[which.min(Video_var)])
+Video_var_min
+
+Video_var_df$Video_var <- round(Video_var_df$Video_var)
+Video_var_df$Video_var <- as.character(Video_var_df$Video_var)
+Video_var_df$Video_var <- formatC(Video_var_df$Video_var, flag = "0", width = 10, format = "d")
+Video_var_df <- Video_var_df[order(Video_var_df$Video_var), ]
+head(Video_var_df)
+
+# outputs video to picture
+foreach(i = seq(1, nrow(Video_var_df), 1)) %dopar% {
+
+	video_file = Video_var_df$video_files[i]
+	Video_var = Video_var_df$Video_var[i]
+
+	name = paste(Video_var, "_", video_file, sep = "")
+
+	print(name)
+
+	data = CCIPCA_video(video_file = video_file, n_components = 10, W = W, H = H, fast = 1)
+
+	Output_image(name = name,
+	 data = data, 
+	 dest_dir = paste(master_dir, "/video_var", sep = "") 
+	 W = W, 
+	 H = H)
 }
 
 beep()
 
-nrow(RepFrames_new)
-ncol(RepFrames_new)
+video_files_new <- as.character(Video_var_min$Min_video_file)
+video_files_new
+
+i = 2
+# cacluates the new rep frames matrix
+if(length(video_files_new) > 0) {
+	RepFrame_new_v <- unlist(mclapply(video_files_new, FUN = function(video_file) CCIPCA_video(video_file = video_file, n_components = 10, W = W, H = H, fast = 0), mc.cores = n_cores))
+	RepFrames_new <- matrix(RepFrame_new_v, ncol = 3*W*H, byrow = TRUE) 
+}
+
+beep()
 
 write.csv(RepFrames_new, "RepFrames_new.csv")
 
@@ -296,14 +347,15 @@ ccipca[["video_files_cur"]] <- video_files_cur
 ccipca[["prx"]] <- prx
 
 # write ccipca list to RData
-setwd("/home/jim/Desktop/video_cluster")
+setwd(master_dir)
 save(ccipca, file = 'ccipca.RData')
 
 # -------------------------------------------------------
 # -------------------------------------------------------
+# plots plot of first 2, and first 3 principal components
 
 # write ccipca list to RData
-setwd("/home/jim/Desktop/video_cluster")
+setwd(master_dir)
 load(file = 'ccipca.RData')
 
 # first two components
@@ -317,10 +369,12 @@ prx <- ccipca[["prx"]]
 prx <- as.data.frame(prx)
 row.names(prx) <- ccipca[["video_files_cur"]]
 
-NbClust <- NbClust(data = prx[, posVar(prx)], min.nc = 2, max.nc = nrow(prx) - 2,
+NbClustN <- NbClust(data = prx[, posVar(prx)], min.nc = 2, max.nc = nrow(prx) - 2,
  method = 'kmeans')
 NbClust$Best.nc[1]
 NbClust
+
+prx[, 1:3]
 
 # ----------------------------------------------------
 # applies hclust clustering
@@ -378,8 +432,14 @@ clusters <- km$cluster
 # n_compenents d
 k <- 10
 km <- kmeans(ccipca[["prx"]], centers = k) 
+
 plot(ccipca[["prx"]][, 1], ccipca[["prx"]][, 2], col = km$cluster)
 points(km$centers[, c(1, 2)], pch = 16)
+text(km$centers[, 1:2], labels = sort(unique(clusters)), pos = 3)
+
+sort(unique(clusters))
+cbind(prx[, 1:2], clusters)
+km$centers
 
 clusters <- km$cluster
 
@@ -389,8 +449,8 @@ clusters <- km$cluster
 # deletes rep_frames folder, then creates it (aviods overlap)
 frame_dir = "/home/jim/Desktop/video_cluster/frames"
 master_dir = "/home/jim/Desktop/video_cluster"
-system(paste('rm -r ', master_dir, '/', 'rep_frames', sep = ""))
-system(paste('mkdir ', master_dir, '/', 'rep_frames', sep = ""))
+system(paste('rm -r ', frame_dir, sep = ""))
+system(paste('mkdir ', frame_dir, sep = ""))
 
 # cycles through clusters
 for(cl in unique(clusters)){
@@ -398,8 +458,8 @@ for(cl in unique(clusters)){
 	print(cl)
 
 	# deletes cluster folder, then creates it (aviods overlap)
-	system(paste('rm -r ', master_dir, '/rep_frames/', cl, sep = ""))
-	system(paste('mkdir ', master_dir, '/rep_frames/', cl, sep = ""))
+	system(paste("rm -r ", frame_dir, "/", cl, sep = ""))
+	system(paste("mkdir ", frame_dir, "/", cl, sep = ""))
 
 	names_cl <- ccipca[["video_files_cur"]][clusters == cl]
 
@@ -408,27 +468,26 @@ for(cl in unique(clusters)){
 		name = names_cl[i]
 		data = ccipca[["RepFrames_cur"]][clusters == cl, ][i, ]
 
-		dest_dir = "/home/jim/Desktop/video_cluster/rep_frames"
+		dest_dir = frames_dir
 		Output_image(name = name, data = data, dest_dir = dest_dir, W = W, H = H)
 
-		dest_dir = paste("/home/jim/Desktop/video_cluster/rep_frames/", cl, sep = "")
+		dest_dir = paste(frame_dir, "/", cl, sep = "")
 		Output_image(name = name, data = data, dest_dir = dest_dir, W = W, H = H)
 
 	}
 }
 
-clusters
 plot(prx[, 1:2], col = clusters)
-
 
 beep()
 
 # ----------------------------------------
-# eigen traps
+# Output Eigentraps
 
-setwd('/home/jim/Desktop/video_cluster/')
+setwd(master_dir)
 load("ccipca.RData")
 
+# number of prinicpal components to output
 n_pc <- 5
 
 pc <- 2
@@ -439,11 +498,112 @@ for(pc in seq(1, n_pc, 1)){
 	data <- ccipca[["components_"]][pc, ]
 	data <- data*255/max(data)
 
-	dest_dir <- "/home/jim/Desktop/video_cluster"
-
-	W <- 192
-	H <- 108
+	W = 192
+	H = 108
+	dest_dir = master_dir
 
 	Output_image(name, data, dest_dir, W, H)
 
+}
+
+# --------------------------------------------------
+# --------------------------------------------------
+# for loop to calculate Video_mod for each Set Trap
+# (this is now contained in CCIPCA_SetTrap)
+
+setwd(video_dir)
+SetTraps <- unique(str_match(sort(list.files(getwd())), "(.+)_GOPR")[, 2])
+
+SetTrap = "STRS2013_S087T014"
+
+n_cores = detectCores(all.tests = FALSE, logical = FALSE) - 1
+n_components = 100
+W = 192
+H = 108
+frame_dir = "/home/jim/Desktop/video_cluster/frame"
+video_dir = "/home/jim/Desktop/video_cluster/video"
+master_dir = "/home/jim/Desktop/video_cluster"
+
+video_file = "STRS2013_S087T014_GOPR0411.MP4"
+
+registerDoParallel(cores = n_cores)
+
+Video_files_mod_df <- matrix(NA, nrow = length(list.files(video_dir)), ncol = 4)
+for(SetTrap in SetTraps) {
+
+	n_components = 300
+	W = 192
+	H = 108
+	skip = 6
+
+	print(SetTrap)
+
+	setwd(video_dir)
+	python.load("CCIPCA_SetTrap.py")
+	output <- python.call("CCIPCA_SetTrap", SetTrap, n_components, W, H, skip)
+	prx <- matrix(unlist(output[[1]]), ncol = sqrt(length(unlist(output[[1]]))), byrow = TRUE)
+	video_files_frames <- output[[2]]
+
+	min = min(as.numeric(str_match(video_files_frames, "(.+)_GOPR0(.+)_F(.+)")[, 3]))
+
+	cols <- as.numeric(str_match(video_files_frames, "(.+)_GOPR0(.+)_F(.+)")[, 3]) - min + 1
+
+	video_files <- na.omit(str_match(video_files_frames, paste("(", SetTrap, ".+)", "_F.+", sep = ""))[, 2])
+	
+	video_files_var <- unlist(lapply(paste(sort(unique(video_files)), ".MP4", sep = ""), FUN = function(video_file) Video_var(video_file, W = W, H = H, skip = 6)))
+
+	video_files_mod <- unlist(lapply(sort(unique(video_files)), FUN = function(video_file) diff_from_mode(prx[video_files == video_file, ])))
+
+	setwd(master_dir)
+	png(paste(SetTrap, ".png", sep = ""))
+	plot(prx[, 1:2], col = cols, main = SetTrap)
+	legend("topright", fill = sort(unique(cols))
+	, legend = paste(unique(sort(cols)) + min - 1, " ", 
+			"Var-", formatC(video_files_var, flag = "0", width = 9, format = "d"), " ", 
+			"Mod-", formatC(video_files_mod, flag = "0", width = 9, format = "d"), sep = ""))
+	dev.off()
+
+	m <- as.matrix(data.frame(SetTrap = rep(SetTrap, length(unique(video_files))), video_files = sort(unique(video_files)), video_var = video_files_var, video_files_mod = video_files_mod), ncol = 4, drop = FALSE)
+
+	Video_files_mod_df[seq(row_ind, row_ind + length(unique(video_files)) - 1, 1), ] = m[, ]
+
+	row_ind <- row_ind + length(unique(video_files))
+
+	print(row_ind)
+}
+
+Video_files_mod_df
+
+# ---------------------------------------
+# animation of video, or settrap
+
+ani <- function(){
+	for(i in seq(1, nrow(prx))){
+		plot(prx[i, 1], prx[i, 2], ylim = range(prx[, 2]), xlim = range(prx[, 1]), main = video_files_frames[i])
+		points(pc1, pc2, col = "red")
+	}
+}
+
+saveGIF(ani())
+
+# ----------------------------------------
+# output frames to jpeg with cluster names in filename
+
+# for given framesData data set, writes each frame to jpeg file
+# adds clustername to start of jpeg file to see which cluster it is in
+for(i in seq(1, length(video_files_frames), 1)) {		
+
+	name = paste(clusters[i], "_", video_files_frames[i], sep = "")
+	data = framesData[i, ]
+	dest_dir = frames_dir
+	W = 192
+	H = 108
+	Output_image(name, data, dest_dir, W, H)
+
+	name = paste(clusters[i], sep = "")
+	data = t(t(components_) %*% fit$centers[clusters[i], ])
+	dest_dir = frames_dir
+	W = 192
+	H = 108
+	Output_image(name, data, dest_dir, W, H)
 }
