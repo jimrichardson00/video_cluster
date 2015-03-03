82164720a0be7e34c90ce449d6ea7d07b4aa7634
Jim Richardson <jimrichardson00>
2015-03-02 16:36:28 -0800

    seperated the Video_training.r into Video_training_data.r to generate data and Video_training.r to train the model. This is the training file. Also if the Output is IfBiotic, then set Inputs as Areas1, Areas2, etc, and subset data to ImageClarity == Clear

--- Video_training.r
+++ Video_training.r
@@ -1,112 +1,17 @@
-year
+setwd(master_dir)
+load(file = paste("data_video", year, ".RData", sep = ""))
 
-# ----------------------------------
-# set outputs and inputs
-# Outputs <- c("DominantSubstrate")
-# Outputs <- c("ImageClarity")
-Inputs <- paste("Video", seq(1, 444, 1), sep = "")
+# ---------------------------------------------
 
-# ------------------------------------------------------
-# load pca data and subset
-
-load(paste("ccipca_video", year, ".RData", sep = ""))
-
-ccipca[["video_files_cur"]]
-
-prx <- ccipca[["prx"]]
-video_files_frames_cur <- ccipca[["video_files_frames_cur"]]
-prx <- as.data.frame(prx)
-names(prx) <- paste("Video", seq(1, ncol(prx), 1), sep = "")
-# convert to numeric
-for(name in names(prx)){
-	prx[, name] <- as.numeric(prx[, name])
-}
-# scale so that variance  = 1
-for(j in 1:ncol(prx)) {
-	if(is.numeric(prx[, j]) == TRUE) {
-		prx[, j] <- prx[, j]/sd(prx[, j])
-	}
-}
 require(stringr)
-prx$Filenames <- str_match(ccipca[["video_files_frames_cur"]], "(.+)_F.+")[, 2]
-clusters <- ccipca[["clusters"]]
-require(stringr)
-Clear <- str_match(list.files(paste(frame_dir, "/Clear", sep = "")), "(.+)\\.jpg")[, 2]
-# Cloudy <- str_match(list.files(paste(frame_dir, "/Cloudy", sep = "")), "(.+)\\.jpg")[, 2]
-ImageClarity <- ifelse(video_files_frames_cur %in% Clear, "Clear", "Cloudy")
-prx$ImageClarity <- ImageClarity
-
-ccipca[["ImageClarity"]] <- ImageClarity
-
-# save.image(paste("ccipca_video", year, ".RData", sep = ""))
-
-# ------------------------------------------------------
-# load video log data
-if(mac == FALSE) {
-
-	require(Hmisc)
-	Sablefish_Survey_2013 <- mdb.get("2013_Sablefish_Survey.mdb")
-	Video_Analysis_Log <- Sablefish_Survey_2013$data
-
-	require(stringr)
-	Video_Analysis_Log$Set <- as.integer(str_match(Video_Analysis_Log$TransectName, "set(.+)camera(.+)")[, 2])
-	Video_Analysis_Log$Set <- formatC(Video_Analysis_Log$Set, width = 4, flag = "0")
-	Video_Analysis_Log$Trap <- as.integer(str_match(Video_Analysis_Log$TransectName, "trap(.+)")[, 2])
-	Video_Analysis_Log$Trap <- formatC(Video_Analysis_Log$Trap, width = 4, flag = "0")
-	Video_Analysis_Log$Filenames <- paste("STRS2013", "_", "S", Video_Analysis_Log$Set, "T", Video_Analysis_Log$Trap, "_", str_match(Video_Analysis_Log$FileName, "(.+)\\.MP4")[, 2], sep = "")
-
-	require(plyr)
-	d <- ddply(.data = Video_Analysis_Log, .variables = .(TransectName, FileName), .fun = summarise, maxID = max(ID))
-	d$Set <- as.integer(str_match(d$TransectName, "set(.+)camera(.+)")[, 2])
-	d$Set <- formatC(d$Set, width = 4, flag = "0")
-	d$Trap <- as.integer(str_match(d$TransectName, "trap(.+)")[, 2])
-	d$Trap <- formatC(d$Trap, width = 4, flag = "0")
-	d$Filenames <- paste("STRS2013", "_", "S", d$Set, "T", d$Trap, "_", str_match(d$FileName, "(.+)\\.MP4")[, 2], sep = "")
-
-	Video_Analysis_Log_m <- merge(x = Video_Analysis_Log, y = d, by.x = "Filenames", by.y = "Filenames", all = FALSE)
-	Video_Analysis_Log_mu <- Video_Analysis_Log_m[Video_Analysis_Log_m$ID == Video_Analysis_Log_m$maxID, ]
-
-	Video_Analysis_Log_mu[is.na(Video_Analysis_Log_mu) == TRUE | Video_Analysis_Log_mu == ""] <- "NA"
-	for(name in names(Video_Analysis_Log_mu)){
-		Video_Analysis_Log_mu[, name] <- as.character(Video_Analysis_Log_mu[, name])
-	}
-
-	Video_Analysis_Log_mu  <- merge(x = Video_Analysis_Log_mu, y = Sablefish_Survey_2013$lu_sounds, by.x = "Sounds", by.y = "SoundsID", all.x = TRUE)
-	Video_Analysis_Log_mu  <- merge(x = Video_Analysis_Log_mu, y = Sablefish_Survey_2013$lu_substrate, by.x = "DominantSubstrate", by.y = "SubstrateId", all.x = TRUE)
-
-	Video_Analysis_Log_mu$SoundName <- gsub("/", "_", Video_Analysis_Log_mu$SoundName)
-
-	Video_Analysis_Log_mu[is.na(Video_Analysis_Log_mu) == TRUE | Video_Analysis_Log_mu == ""] <- "NA"
-	for(name in names(Video_Analysis_Log_mu)){
-		Video_Analysis_Log_mu[, name] <- as.character(Video_Analysis_Log_mu[, name])
-	}
-
-	write.csv(Video_Analysis_Log_mu, "Video_Analysis_Log_mu.csv")
-
-} else {
-
-	Video_Analysis_Log_mu <- read.csv("Video_Analysis_Log_mu.csv")
-
-	Video_Analysis_Log_mu[is.na(Video_Analysis_Log_mu) == TRUE | Video_Analysis_Log_mu == ""] <- "NA"
-	for(name in names(Video_Analysis_Log_mu)){
-		Video_Analysis_Log_mu[, name] <- as.character(Video_Analysis_Log_mu[, name])
-	}
-
+if(c("ImageClarity") %in% Outputs) {
+	Inputs <- na.omit(as.vector(str_match(as.vector(names(data)), "Video.+")))
+} else if (c("IfBiotic" %in% Outputs)) {
+	Inputs <- na.omit(as.vector(str_match(names(data), "Areas.+")))
+	data <- data[data$Filenames %in% Clear, ]
 }
 
-# ------------------------------------------------------
-# merged data set
-data <- merge(x = prx, y = Video_Analysis_Log_mu, by.x = "Filenames", by.y = "Filenames")
-head(data)
-
-save(data, file = paste("data_video", year, ".RData", sep = ""))
-write.csv(data, file = paste("data_video", year, ".csv", sep = ""))
-
-table(data[, Outputs])
-
-if(c("DominantSubstrate") %in% Outputs) {
-	data <- data[data$ImageClarity == "Clear", ]
-}
+# ---------------------------------------------
 
 # unique set, trap combos
 require(stringr)
@@ -129,11 +34,6 @@ for(Output in Outputs) {
 }
 
 # ----------------------------------
-# set outputs and inputs
-# Outputs <- c("DominantSubstrate")
-# Outputs <- c("ImageClarity")
-Inputs <- paste("Video", seq(1, 444, 1), sep = "")
-
 # formula for factors, and for regular
 formula_f <- as.formula(paste(paste(Outputs_f, collapse = "+"), "~", paste(Inputs, collapse = "+")))
 formula <- as.formula(paste(paste(paste("factor(", Outputs, ")", sep = ""), collapse = "+"), "~", paste(Inputs, collapse = "+")))
@@ -142,7 +42,6 @@ formula_f
 formula
 
 # ------------------------------------------------------
-
 # artificial neural network
 require(neuralnet)
 setwd(master_dir)
