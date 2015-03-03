32172c8ae68c1e0bbb0fb27ab9d1ab4943274210
Jim Richardson <jimrichardson00>
2015-03-02 16:26:03 -0800

    code used to read data from RepFrames data frame, then copy it to a jpg image using Output_image. Much faster to copy the file from the frames directory. code used python.exec to do this, rather than a system call, since we also want to remove the trap from the frame. calls file Remove_trap.py directly

--- Video_cluster.r
+++ Video_cluster.r
@@ -92,8 +92,8 @@ save(ccipca, file = paste('ccipca_video', year, '.RData', sep = ''))
 # copies frames into cluster folders, and into rep_frames folder
 
 # # deletes rep_frames folder, then creates it (aviods overlap)
-# system(paste('rm -r ', frame_dir, sep = ""))
-# system(paste('mkdir ', frame_dir, sep = ""))
+system(paste('rm -r ', clust_dir, sep = ""))
+system(paste('mkdir ', clust_dir, sep = ""))
 
 # cycles through clusters
 for(cluster in sort(unique(clusters))) {
@@ -101,21 +101,67 @@ for(cluster in sort(unique(clusters))) {
 	print(cluster)
 
 	# deletes cluster folder, then creates it (aviods overlap)
-	# system(paste("rm -r ", frame_dir, "/", cluster, sep = ""))
-	system(paste("mkdir ", frame_dir, "/", cluster, sep = ""))
+	system(paste("rm -r ", clust_dir, "/", cluster, sep = ""))
+	system(paste("mkdir ", clust_dir, "/", cluster, sep = ""))
 
-	names_cl <- ccipca[["video_files_cur"]][clusters == cluster]
+	video_files_cl <- ccipca[["video_files_cur"]][clusters == cluster]
+	video_files_cl
 
 	for(i in seq(1, sum(clusters == cluster), 1)){
 
-		name = names_cl[i]
-		data = ccipca[["RepFrames_cur"]][clusters == cluster, ][i, ]
+		# name = video_files_cl[i]
+		# data = ccipca[["RepFrames_cur"]][clusters == cluster, ][i, ]
 
-		dest_dir = frame_dir
-		Output_image(name = name, data = data, dest_dir = dest_dir, W = W, H = H)
+		# dest_dir = clust_dir
+		# Output_image(name = name, data = data, dest_dir = dest_dir, W = W, H = H)
 
-		dest_dir = paste(frame_dir, "/", cluster, sep = "")
-		Output_image(name = name, data = data, dest_dir = dest_dir, W = W, H = H)
+		# dest_dir = paste(clust_dir, "/", cluster, sep = "")
+		# Output_image(name = name, data = data, dest_dir = dest_dir, W = W, H = H)
+
+		# video_file = video_files_cl[i]
+
+		# dest_dir = clust_dir
+		# system(paste("cp ", frame_dir, "/", video_file, ".jpg", " ", dest_dir, sep = ""))
+
+		# dest_dir = paste(clust_dir, "/", cluster, sep = "")
+		# system(paste("cp ", frame_dir, "/", video_file, ".jpg", " ", dest_dir, sep = ""))
+
+		video_file = video_files_cl[i]
+		video_file
+
+		dest_dir = clust_dir
+		require(rPython)
+		setwd(master_dir)
+		python.load("Remove_trap.py")
+		python.assign("video_file", video_file)
+		python.assign("frame_dir", frame_dir)
+		python.assign("dest_dir", dest_dir)
+		python.exec("
+		import cv2
+		import os
+		os.chdir(frame_dir)
+		frame = cv2.imread(video_file + '.jpg')
+		frame = Remove_trap( frame )
+		os.chdir(dest_dir)
+		cv2.imwrite(video_file + '.jpg', frame)
+		")
+
+		dest_dir = paste(clust_dir, "/", cluster, sep = "")
+		require(rPython)
+		setwd(master_dir)
+		python.load("Remove_trap.py")
+		python.assign("video_file", video_file)
+		python.assign("frame_dir", frame_dir)
+		python.assign("dest_dir", dest_dir)
+		python.exec("
+		import cv2
+		import os
+		os.chdir(frame_dir)
+		frame = cv2.imread(video_file + '.jpg')
+		frame = Remove_trap( frame )
+		os.chdir(dest_dir)
+		cv2.imwrite(video_file + '.jpg', frame)
+		")
 
 	}
 }
