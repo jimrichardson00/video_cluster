96664b58b7b075b4ae13b9301a66af77d750d057
Jim Richardson <jimrichardson00>
2015-03-02 16:20:35 -0800

    original code was a simpler version of a red blob detector, copied over code from Mean_frame.py. this does a similiar thing, but also detects contours, extracts hue, sat and val values, and max_area.

--- Red_blob.py
+++ Red_blob.py
@@ -3,26 +3,24 @@ import numpy as np
 from decimal import *
 getcontext().prec = 6
 import math
+import os
+import cPickle as pickle
 
-def Red_blob( frame_o ):
+def Red_blob( video_file, frame_o ):
 
-  height, width, depth = frame_o.shape
-
-  x1 = int(math.floor(Decimal(0.26041666666)*Decimal(width)))
-  x2 = int(math.floor(Decimal(0.73958333334)*Decimal(width)))
-  y1 = 0
-  y2 = int(math.floor(Decimal(0.65555555555)*Decimal(height)))
-
-  frame_o = frame_o[y1:y2, x1:x2]
   # frame_o = cv2.resize(frame_o, (W, H))
 
   # smooth it
   frame_o = cv2.blur(frame_o, (3, 3))
 
   # convert to hsv and find range of colors
-  hsv = cv2.cvtColor(frame_o,cv2.COLOR_BGR2HSV)
-  thresh = cv2.inRange(hsv, np.array((120, 0, 0)), np.array((180, 100, 100)))
-  thresh2 = thresh.copy()
+  hsv = cv2.cvtColor(frame_o, cv2.COLOR_BGR2HSV)
+  # thresh1 = cv2.inRange(hsv, np.array((150, 10, 10)), np.array((180, 200, 255)))
+  # thresh2 = cv2.inRange(hsv, np.array((0, 10, 10)), np.array((30, 200, 255)))
+  thresh1 = cv2.inRange(hsv, np.array((150, 5, 5)), np.array((180, 255, 255)))
+  thresh2 = cv2.inRange(hsv, np.array((0, 5, 5)), np.array((30, 255, 255)))
+  thresh = thresh1 + thresh2
+  thresh_c = thresh.copy()
 
   # find contours in the threshold image
   contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -35,7 +33,33 @@ def Red_blob( frame_o ):
           max_area = area
           best_cnt = cnt
 
+  res = cv2.bitwise_and(frame_o, frame_o, mask = thresh)
+ 
   print 'max area = ' + str(max_area)
+
+  hue, sat, val = cv2.split(res)
+
+  if np.nonzero(hue)[0].any() == False:
+    hue0m = 0
+  else:
+    hue0 = hue[np.nonzero(hue)]
+    hue0m = np.amin(hue0)
+
+  if np.nonzero(sat)[0].any() == False:
+    sat0m = 0
+  else:
+    sat0 = sat[np.nonzero(sat)]
+    sat0m = np.amin(sat0)
+
+  if np.nonzero(val)[0].any() == False:
+    val0m = 0
+  else:
+    val0 = val[np.nonzero(val)]
+    val0m = np.amin(val0)
+
+  print "hue range " + str(hue0m) + "-" + str(np.amax(hue))
+  print "sat range " + str(sat0m) + "-" + str(np.amax(sat))
+  print "val range " + str(val0m) + "-" + str(np.amax(val))
 
   # # finding centroids of best_cnt and draw a circle there
   # M = cv2.moments(best_cnt)
@@ -44,9 +68,36 @@ def Red_blob( frame_o ):
 
   # Show it, if key pressed is 'Esc', exit the loop
   cv2.imshow('frame_o',frame_o)
-  cv2.imshow('thresh',thresh2)
+  cv2.imshow('thresh', thresh_c)
 
-  cv2.imwrite("frame_o.jpg", frame_o)
-  cv2.imwrite("thresh2.jpg", thresh2)
+  max_area_c = str(int(round(max_area, 0))).zfill(6)
+  print max_area_c
 
+  red_col = Reduce_colors( frame_o, 2 )
 
+  os.chdir("/home/jim/Dropbox/REM/Tasks/video_cluster/coral2013")
+  cv2.imwrite("O" + video_file + ".jpg", frame_o)
+  # cv2.imwrite("T" + video_file + ".jpg", thresh_c)
+  cv2.imwrite("R" + video_file + ".jpg", res)
+  cv2.imwrite("C" + video_file + ".jpg", red_col)
+
+  hsv_range = {}
+
+  # print doesnt_exist
+
+  hue = np.array(hue)
+  hue = json.loads(json.dumps(hue.tolist()))
+
+  sat = np.array(sat)
+  sat = json.loads(json.dumps(sat.tolist()))
+
+  val = np.array(val)
+  val = json.loads(json.dumps(val.tolist()))
+
+  hsv_range["hue"] = hue
+  hsv_range["sat"] = sat
+  hsv_range["val"] = val
+
+  hsv_range["max_area"] = max_area
+
+  return hsv_range
