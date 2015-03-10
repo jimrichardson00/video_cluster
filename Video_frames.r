e004eeb58a9a3b2403b238455af8da5079bb6def
Jim Richardson <jimrichardson00>
2015-03-09 20:18:23 -0700

    added sort() to code that gets list of video files

--- Video_frames.r
+++ Video_frames.r
@@ -1,18 +1,16 @@
 setwd(master_dir)
 
-video_files = list.files(video_dir)
+video_files = sort(list.files(video_dir))
 video_files
 
 video_file <- video_files[5]
 video_file
 
-Mean_frame <- function(video_file, video_dir, frame_dir, skip) {
+Mean_frame <- function(video_file, video_dir, frame_dir, skip, Hgp, Wgp) {
 
 	setwd(master_dir)
 
 	print(video_file)
-	video_dir
-	video_file
 
 	require(stringr)
 	base_file <- str_match(video_file, "(.+)\\..+4")[, 2]
@@ -24,21 +22,29 @@ Mean_frame <- function(video_file, video_dir, frame_dir, skip) {
 	python.assign("video_dir", video_dir)
 	python.assign("frame_dir", frame_dir)
 	python.assign("skip", skip)
+	python.assign("Hgp", Hgp)
+	python.assign("Wgp", Wgp)
 	python.assign("data", 0)
 	python.load("Remove_trap.py")
 	python.load("Mean_frame.py")
-	python.exec("
-  os.chdir(video_dir)
-  frame_o = Mean_frame( video_file, video_dir, skip, data)
-  os.chdir(frame_dir)
-  cv2.imwrite(base_file + '.jpg', frame_o)
-")
-
+	tryCatch({
+		python.exec("
+		  os.chdir(video_dir)
+		  frame_o = Mean_frame( video_file, video_dir, skip, data, Hgp, Wgp)
+		  os.chdir(frame_dir)
+		  cv2.imwrite(base_file + '.jpg', frame_o)
+		")
+		},
+		error = function(e) {cat("ERROR :",conditionMessage(e), "\n")}
+		)
 }
 
+video_file <- "SKBO2014_S009T003_GOPR0434.MP4"
+Mean_frame(video_file = video_file, video_dir = video_dir, frame_dir = frame_dir, skip = skip, Hgp = Hgp, Wgp = Wgp)
+
 mclapply(video_files, 
-	FUN = function(video_file) Mean_frame(video_file
-		, video_dir = video_dir, frame_dir = frame_dir, skip = skip)
+	FUN = function(video_file) Mean_frame(video_file = video_file
+		, video_dir = video_dir, frame_dir = frame_dir, skip = skip, Hgp = Hgp, Wgp = Wgp)
 	, mc.cores = n_cores
 	, mc.silent = FALSE
 	, mc.preschedule = TRUE
