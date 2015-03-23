4c7bf585480667729f07474d30ffc2c0567d96c5
Jim Richardson <jimrichardson00>
2015-03-23 11:57:00 -0700

    added iteration to output .jpg filename. deleted irrelevent code at end of file

--- Video_projection.r
+++ Video_projection.r
@@ -4,6 +4,9 @@
 setwd(master_dir)
 load(paste("ccipca_video", year, ".RData", sep = ""))
 
+video_files_cur <- ccipca[["video_files_cur"]]
+iteration <- length(video_files_cur)
+
 proje_dim_f = formatC(proje_dim, flag = "0", width = 4)
 system(paste("rm -r ", proje_dir, "/", proje_dim_f, sep = ""))
 system(paste("mkdir ", proje_dir, "/", proje_dim_f, sep = ""))
@@ -18,26 +21,14 @@ prx <- prx[, 1:proje_dim]
 components_ <- components_[1:proje_dim, ]
 
 # projection
-proj = (prx[, 1:proje_dim] %*% components_[1:proje_dim, ]) + matrix(rep(mean_, length(video_files_frames_cur)), nrow = length(video_files_frames_cur), byrow = TRUE)
+proj = (prx[, 1:proje_dim] %*% components_[1:proje_dim, ]) + matrix(rep(mean_, length(video_files_cur)), nrow = length(video_files_cur), byrow = TRUE)
 
 for(i in seq(1, nrow(prx), 1)) {
   
-	video_files_frame = video_files_frames_cur[i]
-
+	video_file = video_files_cur[i]
 	data = proj[i, ]
-  		
-	name = paste("proj", formatC(proje_dim_f, flag = "0", width = 4), "_", video_files_frame, sep = "")
+  name = paste("proj", "I", formatC(iteration, flag = "0", width = 4), "_", formatC(proje_dim_f, flag = "0", width = 4), "_", video_file, sep = "")
 
 	Output_image(name = name, data = data, dest_dir = dest_dir, W = W, H = H)
 
 }
-
-RepFrames_cur <- ccipca[["RepFrames_cur"]]
-
-head(proj[1, ])
-head(RepFrames_cur[1, 1:10])
-
-mean_[1:10]
-
-(prx[, 1:proje_dim] %*% components_[1:proje_dim, ])[1, 1:10]
-
