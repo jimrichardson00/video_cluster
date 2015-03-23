0f995dff8cef4cc626da22cfeb3d34eea2806f82
Jim Richardson <jimrichardson00>
2015-03-23 11:46:22 -0700

    changed output so that name of jpg includes iteration

--- Video_eigenvectors.r
+++ Video_eigenvectors.r
@@ -4,15 +4,17 @@
 setwd(master_dir)
 load(paste("ccipca_video", year, ".RData", sep = ""))
 
+video_files_cur <- ccipca[["video_files_cur"]]
+iteration <- length(video_files_cur)
+
 for(pc in seq(1, n_pc, 1)){
 
-	name <- paste("prcompTWO", formatC(pc, flag = "0", width = 2), sep = "")
+  name <- paste("prcomp", "I", formatC(iteration, flag = "0", width = 4), "_", formatC(pc, flag = "0", width = 4), sep = "")
 
 	data <- ccipca[["components_"]][pc, ]
 	data <- (255/max(data))*data
+	data[data < 0] <- 0
 
-	dest_dir = master_dir
-
-	Output_image(name = name, data, dest_dir, W, H)
+	Output_image(name = name, data = data, dest_dir = eigen_dir, W = W, H = H)
 
 }
