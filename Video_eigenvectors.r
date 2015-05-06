8e9e524a5208ab91bf896de33b76404a19ce418e
Jim Richardson <jimrichardson00>
2015-05-06 14:38:34 -0700

    changed code to components_ directly from .txt files rather than from ccipca list (stored as ccipca_video.RData)

--- Video_eigenvectors.r
+++ Video_eigenvectors.r
@@ -2,16 +2,15 @@
 # Output Eigentraps
 
 setwd(master_dir)
-load(paste("ccipca_video", year, ".RData", sep = ""))
 
-video_files_cur <- ccipca[["video_files_cur"]]
+video_files_cur <- as.vector(read.table(paste("video_files_cur", year, ".txt", sep = ""))[, 1])
 iteration <- length(video_files_cur)
 
 for(pc in seq(1, n_pc, 1)){
 
   name <- paste("prcomp", "I", formatC(iteration, flag = "0", width = 4), "_", formatC(pc, flag = "0", width = 4), sep = "")
 
-	data <- ccipca[["components_"]][pc, ]
+	data <- read.table(paste("components_", year, ".txt", sep = ""))[pc, ]
 	data <- (255/max(data))*data
 	data[data < 0] <- 0
 
