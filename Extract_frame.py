130a32bdc746a60dda6574477e86967e67f4d9b2
Jim Richardson <jimrichardson00>
2015-05-06 14:20:44 -0700

    python code reads data into r from .jpeg files. first needs to remove trap. trap removal parameters were set as particular values, now this can be changed by changing parameters x1, x2, y1, y2, at the start of Video_master.r. basically, added functionality to change the area of trap removal.

--- Extract_frame.py
+++ Extract_frame.py
@@ -11,7 +11,7 @@ import math
 import numpy
 import re
 
-def Extract_frame(video_frame, frame_dir, W, H, skip):
+def Extract_frame(video_frame, frame_dir, W, H, skip, x1, x2, y1, y2):
 
 	# frame_o = numpy.zeros((H, W, 3), numpy.uint8)
  #  frame_o = frame_o.flatten("C").copy()
@@ -21,14 +21,15 @@ def Extract_frame(video_frame, frame_dir, W, H, skip):
   frame_o = cv2.imread(video_frame)
 
   height, width, depth = frame_o.shape
-  x1 = int(math.floor(Decimal(0.26041666666)*Decimal(width)))
-  x2 = int(math.floor(Decimal(0.73958333334)*Decimal(width)))
-  y1 = 0
-  y2 = int(math.floor(Decimal(0.65555555555)*Decimal(height)))
+
+  x1 = int(round(Decimal(x1)*Decimal(width)))
+  x2 = int(round(Decimal(x2)*Decimal(width)))
+  y1 = int(round(Decimal(1.0 - y2)*Decimal(height)))
+  y2 = int(round(Decimal(1.0 - y1)*Decimal(height)))
+
   frame_o = frame_o[y1:y2, x1:x2]
 
   frame_o = cv2.resize(frame_o, (W, H))
-  
   frame_o = frame_o.flatten("C").copy()
   frame_o = numpy.array(frame_o)
   frame_o = numpy.clip(frame_o, 0, 255)
